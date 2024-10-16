154c526b8edcf8fe5b5c0a4a5d91d2984150903d
adung1703 <adung1703@gmail.com>
2024-10-16 20:46:05 +0700

    Create + Register api

---
+++ Backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const registerRoute = require('./routes/register.js');
+
+const DB_URI = 'mongodb+srv://adung1703:Adung_2003@cluster0.klijv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(DB_URI, null)
+  .then(() => {
+    console.log("MongoDB connection successful...");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err.message);
+  })
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/register', registerRoute);
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server đang chạy tại http://localhost:${port}`);
+})
