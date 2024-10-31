64847ebe34ec087876b31637722169587f52c55f
adung1703 <adung1703@gmail.com>
2024-10-31 20:41:38 +0700

    update change

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const { getCourses, getMyCourses, searchCourses, getCourseDetail, addCourse, addChapter } = require('../controllers/courseControllers.js');
 const auth = require('../middlewares/authMiddleware.js');
 
-router.get('/all-courses', getCourses);
+router.post('/all-courses', getCourses);
 router.get('/my-courses', auth, getMyCourses);
 router.get('/search-courses', searchCourses);
 router.get('/get-detail/:courseId', auth, getCourseDetail);
 router.post('/add-course', auth, addCourse);
 router.post('/add-chapter/:courseId', auth, addChapter);
-
 
 module.exports = router;
