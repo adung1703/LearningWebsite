472a830899059204c5e8a3859f8bc4e601f68d38
ntdung411 <ntdung4113@gmail.com>
2024-12-10 15:31:33 +0700

    Fix courseRoutes.js

--- Backend/routes/courseRoutes.js
+++ Backend/routes/courseRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCourses, getMyCourses, searchCourses, findCourseByInstrutor, getCourseDetail, addCourse, updateCourse, deleteCourse, addChapter, updateChapter, deleteChapter } = require('../controllers/courseControllers.js');
+const courseControllers = require('../controllers/courseControllers.js');
 const auth = require('../middlewares/authMiddleware.js');
 
-router.post('/all-courses', getCourses);
-router.get('/my-courses', auth, getMyCourses);
-router.get('/search-courses', searchCourses);
-router.post('/find-by-instructor/', auth, findCourseByInstrutor);
-router.get('/get-detail/:courseId', auth, getCourseDetail);
-router.post('/add-course', auth, addCourse);
-router.put('/update-course/:courseId', auth, updateCourse);
-router.delete('/delete-course/:courseId', auth, deleteCourse);
-router.post('/add-chapter/:courseId', auth, addChapter);
-router.put('/update-chapter/:courseId/:chapterId', auth, updateChapter);
-router.delete('/delete-chapter/:courseId/:chapterId', auth, deleteChapter);
+router.post('/all-courses', courseControllers.getCourses);
+router.get('/my-courses', auth, courseControllers.getMyCourses);
+router.get('/search-courses', courseControllers.searchCourses);
+router.post('/find-by-instructor/', auth, courseControllers.findCourseByInstrutor);
+router.get('/get-detail/:courseId', auth, courseControllers.getCourseDetail);
+router.post('/add-course', auth, courseControllers.addCourse);
+router.put('/update-course/:courseId', auth, courseControllers.updateCourse);
+router.delete('/delete-course/:courseId', auth, courseControllers.deleteCourse);
+router.post('/add-chapter/:courseId', auth, courseControllers.addChapter);
+router.put('/update-chapter/:courseId/:chapterId', auth, courseControllers.updateChapter);
+router.delete('/delete-chapter/:courseId/:chapterId', auth, courseControllers.deleteChapter);
 
 module.exports = router;
