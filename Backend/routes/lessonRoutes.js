accd592a53cc31ad0c51d2860a4693e1330b6ed0
ntdung411 <ntdung4113@gmail.com>
2024-12-10 15:39:34 +0700

    Lesson Controllers and Routes

--- Backend/routes/lessonRoutes.js
+++ Backend/routes/lessonRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { addLesson, getAllLessons, getLesson, getCommentsOfLesson, newComment, replyComment } = require('../controllers/lessonControllers.js');
+const lessonControllers = require('../controllers/lessonControllers.js');
 const auth = require('../middlewares/authMiddleware.js');
 
-router.post('/add-lesson', auth, addLesson);
-router.get('/get-all-lessons', auth, getAllLessons);
-router.get('/get-lesson/:courseId/:lessonId', auth, getLesson);
-router.get('/get-comments-of-lesson/:lessonId', auth, getCommentsOfLesson);
-router.post('/new-comment', auth, newComment);
-router.post('/reply-comment', auth, replyComment);
+router.post('/add-lesson', auth, lessonControllers.addLesson);
+router.get('/get-all-lessons', auth, lessonControllers.getAllLessons);
+router.get('/get-lesson/:courseId/:lessonId', auth, lessonControllers.getLesson);
+router.get('/get-comments-of-lesson/:lessonId', auth, lessonControllers.getCommentsOfLesson);
+router.post('/new-comment', auth, lessonControllers.newComment);
+router.post('/reply-comment', auth, lessonControllers.replyComment);
 
 module.exports = router;
